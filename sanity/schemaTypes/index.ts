63664b7b9f3f4655f6c0afb65125331367649de8
rgrrobinson-pixel <rgrrobinson@gmail.com>
2026-07-15 16:20:55 +1000

    Create index.ts

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,29 @@
+import { type SchemaTypeDefinition } from 'sanity';
+
+import { siteSettings } from './siteSettings';
+import { hero } from './hero';
+import { about } from './about';
+import { service } from './service';
+import { faqItem } from './faqItem';
+import { review } from './review';
+import { gallery } from './gallery';
+import { destinationsGallery } from './destinationsGallery';
+import { videoSection } from './videoSection';
+import { tourPackagesSection } from './tourPackages';
+import { itinerary } from './itinerary';
+import { destination } from './destination';
+
+export const schemaTypes: SchemaTypeDefinition[] = [
+  siteSettings,
+  hero,
+  about,
+  service,
+  tourPackagesSection,
+  faqItem,
+  review,
+  gallery,
+  destinationsGallery,
+  videoSection,
+  itinerary,
+  destination,
+];
